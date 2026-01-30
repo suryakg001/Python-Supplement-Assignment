68cc2fd3cba67284ecdfc9a03195b549b1e62098
suryakg001 <suryakg001@gmail.com>
2026-01-30 15:36:27 +0530

    Update problem_76.py

--- problem_76.py
+++ problem_76.py
@@ -1,12 +1,9 @@
 # Problem 76: Calculate string similarity (common characters)
 # Find and fix the error
 
+
 def string_similarity(str1, str2):
-    common = 0
-    for char in str1:
-        if char in str2:
-            common += 1
-    return common
+    return len(set(str1) & set(str2))
 
 s1 = "hello"
 s2 = "world"
