28831adb9f11a2b78d438b4973087dd0ac1970af
suryakg001 <suryakg001@gmail.com>
2026-01-30 15:45:06 +0530

    Update problem_83.py

--- problem_83.py
+++ problem_83.py
@@ -3,7 +3,7 @@
 
 def kth_smallest(arr, k):
     sorted_arr = sorted(arr)
-    return sorted_arr[k]
+    return sorted_arr[k - 1]  # subtract 1 for 0-based index
 
 numbers = [7, 10, 4, 3, 20, 15]
 print(f"3rd smallest: {kth_smallest(numbers, 3)}")
