420986305c61c1b42b0b3a7b18a1d9eb2f0e8f7d
suryakg001 <suryakg001@gmail.com>
2026-01-30 15:17:46 +0530

    Update problem_54.py

--- problem_54.py
+++ problem_54.py
@@ -2,10 +2,12 @@
 # Find and fix the error
 
 def nth_fibonacci(n):
-    if n <= 1:
-        return n
-    a, b = 0, 1
-    for i in range(2, n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    a, b = 1, 1  # F1 = 1, F2 = 1
+    for i in range(3, n + 1):
         a, b = b, a + b
     return b
 
